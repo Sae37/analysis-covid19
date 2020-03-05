6a2d6e227185ce7e9793126cf4a3f62a22af244d
Sae37 <57757279+Sae37@users.noreply.github.com>
2020-03-05 23:32:51 +0000

    Add files via upload

--- pdf_import.py
+++ pdf_import.py
@@ -1,11 +1,5 @@
-import os
 import tabula
 
-## pdf file location
-## file = "20200121_COVID-19_SR001.pdf"
-
-## Add relative path to py script to provide full path for pdf file.
-file = os.path.join(sys.path[0], "20200121_COVID-19_SR001.pdf")
-print(file)
+file = "20200121_COVID-19_SR001.pdf"
 
 tables = tabula.read_pdf(file, pages = "all", multiple_tables = True)
